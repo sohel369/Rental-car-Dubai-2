fe3cda636d71da8a1f2bfbca105549b4c7bb27f4
Sohel Rana <sohel0130844@gmail.com>
2026-03-06 12:49:26 +0600

    img

--- src/data/cars.ts
+++ src/data/cars.ts
@@ -29,7 +29,7 @@ export const carsData: Car[] = [
         doors: 4,
         transmission: 'Automatic',
         fuel: 'Petrol',
-        image: '/images/cars/camry.png',
+        image: 'https://images.unsplash.com/photo-1617788138017-80ad40651399?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         available: true,
         rating: 4.8,
         reviews: 124,
